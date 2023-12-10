763910cd0df8d2c3a7f815c2cd8e75665c8bbf49
tushar-nath <tusharnath10@gmail.com>
2023-12-10 14:25:54 +0530

    chore: updated navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,43 @@
 import React from "react";
+import { IoSettingsOutline, IoNotificationsOutline } from "react-icons/io5";
+import ProfileImg from "../images/pfp.jpg";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-100 p-4 h-24">
-      <div className="container mx-auto">
-        <div className="text-black">Your Dashboard Name</div>
-        {/* Add other navbar content here */}
+    <nav className="bg-gray-100 p-2 h-24">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Welcome Message */}
+        <div className="ml-1">
+          <div className="text-4xl h-5 h-12">Welcome Tushar,</div>
+          <div className="text-lg">Here is a summary of your business</div>
+        </div>
+
+        {/* Icons */}
+        <div className="flex ml-96">
+          <div className="mr-4 bg-gray-200 h-16 w-16 flex items-center justify-center rounded-full">
+            <IoSettingsOutline size={32} className="text-2xl" />
+          </div>
+          <div className="bg-gray-200 h-16 w-16 flex items-center justify-center rounded-full">
+            <IoNotificationsOutline size={32} className="text-2xl" />
+          </div>
+        </div>
+
+        {/* Name and Email */}
+        <div>
+          <div className="flex gap-5 w-72">
+            <img
+              src={ProfileImg}
+              alt="profile"
+              className="rounded-full h-16 w-16"
+            />
+            <div className="flex-column">
+              <div className="h-8 text-2xl items-center justify-center">
+                Tushar Nath
+              </div>
+              <div>tusharnath10@gmail.com</div>
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
   );
